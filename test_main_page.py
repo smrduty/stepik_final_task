f2f9d61c5496d887a0fbc833758c4b7828bc06a4
vanya <vanya.fedorov.14@mail.ru>
2023-08-04 18:17:32 +0300

    4.2 step 6

--- test_main_page.py
+++ test_main_page.py
@@ -14,4 +14,9 @@ def test_guest_can_go_to_login_page(browser):
     #browser.get(link)
     #login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     #login_link.click()
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
