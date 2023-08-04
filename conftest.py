580b15fd968bc3e7ba9f5b029bb525a2dc0e54c6
vanya <vanya.fedorov.14@mail.ru>
2023-08-04 15:07:50 +0300

    1st commit for 4.1 stepik selenium course

---
+++ conftest.py
@@ -0,0 +1,22 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action = 'store', default = 'en', \
+                     help = "Choose browser language")
+
+@pytest.fixture(scope="function")
+def browser(request):
+    language = request.config.getoption("language")
+    options = Options()
+    options.add_experimental_option('prefs', \
+                                    {'intl.accept_languages': language})
+    print("\nstarting browser for test..")
+    browser = webdriver.Chrome(options=options)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
