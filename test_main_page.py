92a5e801415c09288d8a2d70e308407b036d34db
vanek <vanya.fedorov.14@mail.ru>
2023-08-06 20:09:20 +0300

    4.2 completed

--- test_main_page.py
+++ test_main_page.py
@@ -6,43 +6,42 @@ from .pages.login_page import LoginPage
     #login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
     #login_link.click()
 
-def test_guest_can_go_to_login_page(browser):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-
 def test_guest_should_see_login_link(browser):
-    #link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
-def test_login_url_should_be_correct(browser):
+def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.go_to_login_page()
-    page = LoginPage(browser, link)
-    page.should_be_login_url()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
 
-def test_login_form_exist(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    page = LoginPage(browser, link)
-    page.should_be_login_form()
+# def test_login_url_should_be_correct(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     page = LoginPage(browser, link)
+#     page.should_be_login_url()
 
-def test_register_form_exist(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    page = LoginPage(browser, link)
-    page.should_be_register_form()
+# def test_login_form_exist(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     page = LoginPage(browser, link)
+#     page.should_be_login_form()
 
+# def test_register_form_exist(browser):
+#     link = "http://selenium1py.pythonanywhere.com/"
+#     page = MainPage(browser, link)
+#     page.open()
+#     page.go_to_login_page()
+#     page = LoginPage(browser, link)
+#     page.should_be_register_form()
 
 
